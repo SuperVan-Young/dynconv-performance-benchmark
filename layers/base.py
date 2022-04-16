98fcecd403bc9c2100e088553ae0117607466cd4
Zhihang Yuan <yuanzhihang1@126.com>
2022-04-16 08:08:48 +0000

    modified base; gather

--- layers/base.py
+++ layers/base.py
@@ -1,10 +1,8 @@
 from abc import abstractmethod
 import numpy as np
-import torch
 import tvm
 from tvm import autotvm
 import os
-import logging
 
 
 class BaseLayerScheduler():
@@ -12,7 +10,6 @@ class BaseLayerScheduler():
     """
 
     def __init__(self):
-        self.task_name = None  # registered name of tvm task
         self.target = "cuda"  # default target is cuda
         self.device = tvm.device("cuda", 0)  # default device is GPU:0
         self.n_trial = 100  # number of trials in autotuning
@@ -25,41 +22,76 @@ class BaseLayerScheduler():
     def __repr__(self) -> str:
         return "BaseLayerScheduler"
 
-    @autotvm.template("dynconv/baselayer")
-    @abstractmethod
     @property
+    @abstractmethod
     def _schedule(self):
         """define operation's schedule and dataflow
         """
         pass
 
-    @abstractmethod
     @property
-    def task_args(self):
-        """arguments that autotvm uses to create the corresponding task
+    @abstractmethod
+    def _task_name(self):
+        """autotvm uses this to find the template
+        """
+
+    @property
+    @abstractmethod
+    def _task_args(self):
+        """autotvm uses these arguments to create a scheduling task
         """
 
     @abstractmethod
-    def generate_sample(self):
-        """generate a random sample in numpy
+    def _generate_sample(self):
+        """generate a random sample.
+
+        Don't forget to EXPLICITLY point out the ndarray's dtype
+
+        Returns:
+            list: inner elements are all numpy.ndarray
+        """
+        return None
+    
+    @abstractmethod
+    def _convert_sample(self, sample):
+        """Convert sample so that runtime function could directly use it
+
+        Args:
+            sample (list): randomly generated sample in numpy ndarray format
+
+        Returns:
+            list: a list of both input and output tvm.nd.arrays that could
+            be sent to tvm runtime function directly
         """
         return None
 
     @abstractmethod
-    def _run_numpy(self, args):
+    def _run_tvm(self, inputs):
         raise NotImplementedError
 
     @abstractmethod
-    def _run_pytorch(self, args):
+    def _run_numpy(self, inputs):
         raise NotImplementedError
 
-    def autotune(self):
+    @abstractmethod
+    def _run_pytorch(self, inputs):
+        raise NotImplementedError
+
+    def autotune(self, refresh=False):
         """Autotune the given schedule and log the best one
+
+        Args:
+            refresh (bool, optional): refresh the log. Default to False.
         """
+
         # TODO: add logging
+        assert self.save_path != None, "Give a saving path for autotuning!"
+
+        if os.path.exists(self.save_path) and refresh:
+            os.remove(self.save_path)
 
         task = autotvm.task.create(
-            task_name=self.task_name, args=self.task_args, target=self.target
+            task_name=self._task_name, args=self._task_args, target=self.target
         )
 
         measure_option = autotvm.measure_option(
@@ -74,17 +106,18 @@ class BaseLayerScheduler():
             callbacks=[autotvm.callback.log_to_file(self.save_path)],
         )
 
-
-    def build(self):
+    def build(self, display=False):
         """Build a runtime function on the best schedule
         """
         with autotvm.apply_history_best(self.save_path):
             with tvm.target.Target(self.target):
-                s, arg_bufs = self._schedule()
+                s, arg_bufs = self._schedule
                 self.func = tvm.build(s, arg_bufs)
+                if display:
+                    print(tvm.lower(s, arg_bufs, simple_mode=True))
 
     def run(self, inputs, runtype="tvm"):
-        """Run tvm runtime function or numpy/pytorch benchmark
+        """Run tvm runtime function or numpy/pytorch benchmark.
 
         Args:
             inputs (list of numpy ndarrays): input arguments
@@ -98,9 +131,8 @@ class BaseLayerScheduler():
         """
         result = None
         if runtype == "tvm":
-            assert self.func != None, "You should a tvm runtime function first."
-            inputs = [tvm.nd.array(arr, self.device) for arr in inputs]
-            result = self.func(*inputs).numpy()
+            assert self.func is not None, "You should a tvm runtime function first."
+            result = self._run_tvm(inputs)
         elif runtype == "numpy":
             result = self._run_numpy(inputs)
         elif runtype == "pytorch":
@@ -119,7 +151,7 @@ class BaseLayerScheduler():
             runtype (str, optional): benchmark runtype. Defaults to "numpy".
         """
         assert runtype != "tvm", "'tvm' shouldn't be benchmark runtype"
-        sample = self.generate_sample()
+        sample = self._generate_sample()
         valid = self.run(sample, runtype)
         result = self.run(sample, "tvm")
         tvm.testing.assert_allclose(valid, result, rtol=self.rtol)
@@ -130,8 +162,9 @@ class BaseLayerScheduler():
         Returns:
             float: average running time (in seconds)
         """
-        assert self.func != None, "You should a tvm runtime function first."
-        sample = [tvm.nd.array(_, self.device) for _ in self.generate_sample()]
+        assert self.func is not None, "You should a tvm runtime function first."
+        sample = self._generate_sample()
+        sample = self._convert_sample(sample)
         evaluator = self.func.time_evaluator(
             self.func.entry_name, self.device, number=self.eval_repeat)
         return evaluator(*sample).mean
---
+++ layers/gather.py
@@ -0,0 +1,140 @@
+import numpy as np
+import tvm
+from tvm import autotvm
+import tvm.te as te
+import tvm.testing
+
+from .base import BaseLayerScheduler
+
+
+@autotvm.template("dynconv/gather")
+def schedule_gather(C, H, W, L, G):
+    # dataflow
+    input = te.placeholder((1, C, H, W), dtype="float32", name="input")
+    rows = te.placeholder((L, ), dtype="int32", name="rows")
+    cols = te.placeholder((L, ), dtype="int32", name="cols")
+
+    input_padded = te.compute(
+        (1, C, H+2, W+2),
+        lambda n, c, y, x: tvm.tir.if_then_else(
+            tvm.tir.all(y >= 1, y - 1 < H, x >= 1, x - 1 < W),
+            input[n, c, y - 1, x - 1],
+            tvm.tir.const(0.0)),
+        name="input_padded")
+
+    def vec2row(row, x): return row * G + x // (G + 2)
+    def vec2col(col, x): return col * G + x % (G + 2)
+
+    output = te.compute(
+        (L, C, (G+2)*(G+2)),
+        lambda b, c, x: input_padded[0, c, vec2row(
+            rows[b], x), vec2col(cols[b], x)],
+        name="output")
+
+    s = te.create_schedule(output.op)
+
+    # scheduling
+    cfg = autotvm.get_config()
+
+    n, f, x = s[output].op.axis
+    cfg.define_split("tile_n", n, num_outputs=3)
+    cfg.define_split("tile_f", f, num_outputs=4)
+    cfg.define_split("tile_x", x, num_outputs=3)
+
+    # caching
+    s[input_padded].compute_inline()
+    OL = s.cache_write(output, "local")
+
+    # tile and bind spatial axes
+    bn, tn, ni = cfg["tile_n"].apply(s, output, n)  # don't need vn
+    bf, vf, tf, fi = cfg["tile_f"].apply(s, output, f)
+    vx, tx, xi = cfg["tile_x"].apply(s, output, x)  # 1 thraed-block for a grid
+
+    s[output].bind(bn, te.thread_axis("blockIdx.y"))
+    s[output].bind(bf, te.thread_axis("blockIdx.x"))
+    s[output].bind(vf, te.thread_axis("vthread"))
+    s[output].bind(vx, te.thread_axis("vthread"))
+    s[output].bind(tn, te.thread_axis("threadIdx.z"))
+    s[output].bind(tf, te.thread_axis("threadIdx.y"))
+    s[output].bind(tx, te.thread_axis("threadIdx.x"))
+
+    s[output].reorder(bn, bf, vf, vx, tn, tf, tx, ni, fi, xi)
+
+    s[OL].compute_at(s[output], tx)
+
+    return s, [input, rows, cols, output]
+
+
+class GatherScheduler(BaseLayerScheduler):
+    def __init__(self, width, channel, sparselen, granularity, save_path):
+        super().__init__()
+        assert width % granularity == 0
+        assert sparselen < width * width
+
+        self.width = width
+        self.channel = channel
+        self.sparselen = sparselen
+        self.granularity = granularity
+        self.save_path = save_path
+
+        self.task_name = "dynconv/gather"
+
+    def __repr__(self) -> str:
+        return "GatherScheduler"
+
+    @property
+    def _task_name(self):
+        return "dynconv/gather"
+
+    @property
+    def _task_args(self):
+        return [self.channel, self.width, self.width, self.sparselen, self.granularity]
+
+    @property
+    def _schedule(self):
+        return schedule_gather(*self._task_args)
+
+    def _generate_sample(self):
+        H, W = self.width, self.width
+        C = self.channel
+        L = self.sparselen
+        G = self.granularity
+
+        input = np.random.randn(1, C, H, W).astype("float32")
+        perm = np.arange(0, (H//G)*(W//G))
+        np.random.shuffle(perm)
+        rows = (perm[:L] // (W//G)).astype("int32")
+        cols = (perm[:L] % (W//G)).astype("int32")
+
+        return [input, rows, cols]
+
+    def _convert_sample(self, sample):
+        L, C, G = self.sparselen, self.channel, self.granularity
+        sample = [tvm.nd.array(_, self.device) for _ in sample]
+        output = tvm.nd.empty((L, C, (G+2)*(G+2)), "float32", self.device)
+        sample.append(output)
+        return sample
+
+    def _run_tvm(self, inputs):
+        inputs = self._convert_sample(inputs)
+        self.func(*inputs)
+        return inputs[-1].numpy()
+
+    def _run_numpy(self, inputs):
+        C = self.channel
+        L = self.sparselen
+        G = self.granularity
+
+        input, rows, cols = inputs
+        input_pad = np.pad(input, ((0, 0), (0, 0), (1, 1),
+                           (1, 1)), mode="constant", constant_values=0)
+        output = np.zeros((L, C, G+2, G+2), dtype="float32")
+
+        for i in range(L):
+            rg = rows[i] * G
+            rc = cols[i] * G
+            output[i:i+1, :, :, :] = input_pad[:, :, rg:rg+G+2, rc:rc+G+2]
+        return output.reshape((L, C, -1))
+
+    def _run_pytorch(self, inputs):
+        raise NotImplementedError
