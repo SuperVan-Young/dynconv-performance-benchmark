22a7714713c494c2bac56506434341d5c8ababcb
Zhihang Yuan <yuanzhihang1@126.com>
2022-04-17 08:33:12 +0000

    support conv dense with different kernel size

--- layers/conv_dense.py
+++ layers/conv_dense.py
@@ -9,18 +9,20 @@ import torch.nn.functional as F
 from .base import BaseLayerScheduler
 
 
-@autotvm.template("dynconv/conv1x1_dense")
-def schedule_conv1x1_dense(Cout, Cin, H, W):
+@autotvm.template("dynconv/conv_dense")
+def schedule_conv_dense(Cout, Cin, H, W, KH, KW):
     # dataflow
     input = te.placeholder((1, Cin, H, W), dtype="float32", name="input")
-    weight = te.placeholder((Cout, Cin, 1, 1), dtype="float32", name="weight")
+    weight = te.placeholder((Cout, Cin, KH, KW), dtype="float32", name="weight")
 
-    rc = te.reduce_axis((0, Cin), name="rc")
+    rc = te.reduce_axis((0, Cin), "rc")
+    ry = te.reduce_axis((0, KH), "ry")
+    rx = te.reduce_axis((0, KW), "rx")
     output = te.compute(
-        (1, Cout, H, W),
+        (1, Cout, H-KH+1, W-KW+1),
         lambda n, f, y, x: te.sum(
-            input[n, rc, y, x] * weight[f, rc, 0, 0],
-            axis=[rc, ]
+            input[n, rc, y + ry, x + rx] * weight[f, rc, ry, rx],
+            axis=[rc, ry, rx]
         ),
         name="output"
     )
@@ -35,6 +37,8 @@ def schedule_conv1x1_dense(Cout, Cin, H, W):
     cfg.define_split("tile_y", y, num_outputs=4)  # by relieve memory ref
     cfg.define_split("tile_x", x, num_outputs=4)  # bx relieve memory ref
     cfg.define_split("tile_rc", rc, num_outputs=3)
+    cfg.define_split("tile_ry", ry, num_outputs=3)
+    cfg.define_split("tile_rx", rx, num_outputs=3)
 
     # caching
     OL = s.cache_write(output, "local")
@@ -63,21 +67,23 @@ def schedule_conv1x1_dense(Cout, Cin, H, W):
 
     # tile reduction axes
     n, f, y, x = s[OL].op.axis
-    [rc, ] = s[OL].op.reduce_axis
+    rc, ry, rx = s[OL].op.reduce_axis
     rco, rcm, rci = cfg["tile_rc"].apply(s, OL, rc)
-    s[OL].reorder(rco, rcm, rci, n, f, y, x)
-    s[AA].compute_at(s[OL], rco)
-    s[WW].compute_at(s[OL], rco)
-    s[AL].compute_at(s[OL], rcm)
-    s[WL].compute_at(s[OL], rcm)
+    ryo, rym, ryi = cfg["tile_ry"].apply(s, OL, ry)
+    rxo, rxm, rxi = cfg["tile_rx"].apply(s, OL, rx)
+    s[OL].reorder(rco, ryo, rxo, rcm, rym, rxm, rci, ryi, rxi, n, f, y, x)
+    s[AA].compute_at(s[OL], rxo)
+    s[WW].compute_at(s[OL], rxo)
+    s[AL].compute_at(s[OL], rxm)
+    s[WL].compute_at(s[OL], rxm)
 
     # cooperative fetching
     for load in [AA, WW]:
         n, f, y, x = s[load].op.axis
         fused = s[load].fuse(n, f, y, x)
         tz, fused = s[load].split(fused, nparts=cfg["tile_f"].size[2])  # tf
-        ty, fused = s[load].split(fused, nparts=cfg["tile_y"].size[1])  # ty
-        tx, fused = s[load].split(fused, nparts=cfg["tile_x"].size[1])  # tx
+        ty, fused = s[load].split(fused, nparts=cfg["tile_y"].size[2])  # ty
+        tx, fused = s[load].split(fused, nparts=cfg["tile_x"].size[2])  # tx
         s[load].bind(tz, te.thread_axis("threadIdx.z"))
         s[load].bind(ty, te.thread_axis("threadIdx.y"))
         s[load].bind(tx, te.thread_axis("threadIdx.x"))
@@ -85,43 +91,46 @@ def schedule_conv1x1_dense(Cout, Cin, H, W):
     return s, [input, weight, output]
 
 
-class Conv1x1DenseScheduler(BaseLayerScheduler):
-    def __init__(self, channel_out, channel_in, width, save_path):
+class ConvDenseScheduler(BaseLayerScheduler):
+    def __init__(self, channel_out, channel_in, width, kernel, save_path):
         super().__init__()
 
         self.channel_out = channel_out
         self.channel_in = channel_in
         self.width = width
+        self.kernel = kernel
         self.save_path = save_path
 
     def __repr__(self) -> str:
-        return "Conv1x1DenseScheduler"
+        return "ConvDenseScheduler"
 
     @property
     def _task_name(self):
-        return "dynconv/conv1x1_dense"
+        return "dynconv/conv_dense"
 
     @property
     def _task_args(self):
-        return [self.channel_out, self.channel_in, self.width, self.width]
+        return [self.channel_out, self.channel_in, self.width, self.width, self.kernel, self.kernel]
 
     @property
     def _schedule(self):
-        return schedule_conv1x1_dense(*self._task_args)
+        return schedule_conv_dense(*self._task_args)
 
     def _generate_sample(self):
         H, W = self.width, self.width
+        KH, KW = self.kernel, self.kernel
         Cout, Cin = self.channel_out, self.channel_in
         input = np.random.randn(1, Cin, H, W).astype("float32")
-        weight = np.random.randn(Cout, Cin, 1, 1).astype("float32")
+        weight = np.random.randn(Cout, Cin, KH, KW).astype("float32")
         return [input, weight]
 
     def _convert_sample(self, sample):
         H, W = self.width, self.width
+        KH, KW = self.kernel, self.kernel
         Cout, Cin = self.channel_out, self.channel_in
         sample = [tvm.nd.array(_, self.device) for _ in sample]
         output = tvm.nd.empty(
-            (1, Cout, H, W), dtype="float32", device=self.device)
+            (1, Cout, H-KH+1, W-KW+1), dtype="float32", device=self.device)
         sample.append(output)
         return sample
 
@@ -134,8 +143,6 @@ class Conv1x1DenseScheduler(BaseLayerScheduler):
         raise NotImplementedError
 
     def _run_pytorch(self, inputs):
-        H, W = self.width, self.width
-        Cout, Cin = self.channel_out, self.channel_in
         input, weight = inputs
         input = torch.tensor(input, dtype=torch.float32, device="cuda")
         weight = torch.tensor(weight, dtype=torch.float32, device="cuda")
