e38d89e61a2b842ab89e3023bd89d5cd55607ebb
Zhihang Yuan <yuanzhihang1@126.com>
2022-04-17 07:04:03 +0000

    accelerate conv1x1 dense

--- layers/conv1x1_dense.py
+++ layers/conv1x1_dense.py
@@ -31,9 +31,9 @@ def schedule_conv1x1_dense(Cout, Cin, H, W):
     cfg = autotvm.get_config()
     n, f, y, x = s[output].op.axis
     # no tile on n
-    cfg.define_split("tile_f", f, num_outputs=4) # with fi
-    cfg.define_split("tile_y", y, num_outputs=3)
-    cfg.define_split("tile_x", x, num_outputs=3)
+    cfg.define_split("tile_f", f, num_outputs=4)  # with fi
+    cfg.define_split("tile_y", y, num_outputs=4)  # by relieve memory ref
+    cfg.define_split("tile_x", x, num_outputs=4)  # bx relieve memory ref
     cfg.define_split("tile_rc", rc, num_outputs=3)
 
     # caching
@@ -46,18 +46,19 @@ def schedule_conv1x1_dense(Cout, Cin, H, W):
 
     # tile and bind spatial axes
     bf, vf, tf, fi = cfg["tile_f"].apply(s, output, f)
-    vy, ty, yi = cfg["tile_y"].apply(s, output, y)
-    vx, tx, xi = cfg["tile_x"].apply(s, output, x)
+    by, vy, ty, yi = cfg["tile_y"].apply(s, output, y)
+    bx, vx, tx, xi = cfg["tile_x"].apply(s, output, x)
 
-    s[output].bind(n, te.thread_axis("blockIdx.y"))
-    s[output].bind(bf, te.thread_axis("blockIdx.x"))
+    s[output].bind(bf, te.thread_axis("blockIdx.z"))
+    s[output].bind(by, te.thread_axis("blockIdx.y"))
+    s[output].bind(bx, te.thread_axis("blockIdx.x"))
     s[output].bind(vf, te.thread_axis("vthread"))
     s[output].bind(vy, te.thread_axis("vthread"))
     s[output].bind(vx, te.thread_axis("vthread"))
     s[output].bind(tf, te.thread_axis("threadIdx.z"))
     s[output].bind(ty, te.thread_axis("threadIdx.y"))
     s[output].bind(tx, te.thread_axis("threadIdx.x"))
-    s[output].reorder(n, bf, vf, vy, vx, tf, ty, tx, fi, yi, xi)
+    s[output].reorder(n, bf, by, bx, vf, vy, vx, tf, ty, tx, fi, yi, xi)
     s[OL].compute_at(s[output], tx)
 
     # tile reduction axes
@@ -82,6 +83,7 @@ def schedule_conv1x1_dense(Cout, Cin, H, W):
         s[load].bind(tx, te.thread_axis("threadIdx.x"))
 
     return s, [input, weight, output]
+
 
 class Conv1x1DenseScheduler(BaseLayerScheduler):
     def __init__(self, channel_out, channel_in, width, save_path):
@@ -113,15 +115,16 @@ class Conv1x1DenseScheduler(BaseLayerScheduler):
         input = np.random.randn(1, Cin, H, W).astype("float32")
         weight = np.random.randn(Cout, Cin, 1, 1).astype("float32")
         return [input, weight]
-    
+
     def _convert_sample(self, sample):
         H, W = self.width, self.width
         Cout, Cin = self.channel_out, self.channel_in
         sample = [tvm.nd.array(_, self.device) for _ in sample]
-        output = tvm.nd.empty((1, Cout, H, W), dtype="float32", device=self.device)
+        output = tvm.nd.empty(
+            (1, Cout, H, W), dtype="float32", device=self.device)
         sample.append(output)
         return sample
-    
+
     def _run_tvm(self, inputs):
         inputs = self._convert_sample(inputs)
         self.func(*inputs)
@@ -129,7 +132,7 @@ class Conv1x1DenseScheduler(BaseLayerScheduler):
 
     def _run_numpy(self, inputs):
         raise NotImplementedError
-    
+
     def _run_pytorch(self, inputs):
         H, W = self.width, self.width
         Cout, Cin = self.channel_out, self.channel_in
