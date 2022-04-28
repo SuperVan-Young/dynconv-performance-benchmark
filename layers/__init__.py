77506bbe5af11e1bbed4847782335b52139533e2
Zhihang Yuan <yuanzhihang1@126.com>
2022-04-28 08:43:27 +0000

    group conv dense, pass validity check

--- layers/__init__.py
+++ layers/__init__.py
@@ -3,6 +3,6 @@ from .gather import GatherScheduler
 from .conv3x3_gathered import Conv3x3GatheredScheduler
 from .scatter_add import ScatterAddScheduler
 from .conv1x1_gathered import Conv1x1GatheredScheduler
-from .conv_dense import ConvDenseScheduler
+from .conv_dense import ConvDenseScheduler, GroupConvDenseScheduler
 from .add import AddScheduler
 from .pooling import PoolingScheduler
--- layers/conv_dense.py
+++ layers/conv_dense.py
@@ -1,4 +1,5 @@
 import numpy as np
+from pyparsing import Group
 import tvm
 from tvm import autotvm
 import tvm.te as te
@@ -150,5 +151,139 @@ class ConvDenseScheduler(BaseLayerScheduler):
         input = torch.tensor(input, dtype=torch.float32, device="cuda")
         weight = torch.tensor(weight, dtype=torch.float32, device="cuda")
         output = F.conv2d(input, weight)
+        output = output.cpu().numpy()
+        return output
+
+@autotvm.template("dynconv/group_conv_dense")
+def schedule_group_conv_dense(Cout, Cin, H, W, KH, KW, group):
+    # dataflow
+    input = te.placeholder((1, Cin, H, W), dtype="float32", name="input")
+    weight = te.placeholder((Cout, Cin//group, KH, KW), dtype="float32", name="weight")
+
+    def find_group(f): return (f // (Cout // group)) * (Cin // group)
+
+    rc = te.reduce_axis((0, Cin//group), "rc")
+    ry = te.reduce_axis((0, KH), "ry")
+    rx = te.reduce_axis((0, KW), "rx")
+    output = te.compute(
+        (1, Cout, H-KH+1, W-KW+1),
+        lambda n, f, y, x: te.sum(
+            input[n, find_group(f)+rc, y + ry, x + rx] * weight[f, rc, ry, rx],
+            axis=[rc, ry, rx]
+        ),
+        name="output"
+    )
+
+    s = te.create_schedule(output.op)
+
+    # schedule
+    cfg = autotvm.get_config()
+    n, f, y, x = s[output].op.axis
+    # no tile on n
+    cfg.define_split("tile_f", f, num_outputs=4)  # with fi
+    cfg.define_split("tile_y", y, num_outputs=4)  # by relieve memory ref
+    cfg.define_split("tile_x", x, num_outputs=4)  # bx relieve memory ref
+    cfg.define_split("tile_rc", rc, num_outputs=3)
+    cfg.define_split("tile_ry", ry, num_outputs=3)
+    cfg.define_split("tile_rx", rx, num_outputs=3)
+
+    # caching
+    OL = s.cache_write(output, "local")
+
+    AA = s.cache_read(input, "shared", [OL])
+    WW = s.cache_read(weight, "shared", [OL])
+    AL = s.cache_read(AA, "local", [OL])
+    WL = s.cache_read(WW, "local", [OL])
+
+    # tile and bind spatial axes
+    bf, vf, tf, fi = cfg["tile_f"].apply(s, output, f)
+    by, vy, ty, yi = cfg["tile_y"].apply(s, output, y)
+    bx, vx, tx, xi = cfg["tile_x"].apply(s, output, x)
+
+    s[output].bind(bf, te.thread_axis("blockIdx.z"))
+    s[output].bind(by, te.thread_axis("blockIdx.y"))
+    s[output].bind(bx, te.thread_axis("blockIdx.x"))
+    s[output].bind(vf, te.thread_axis("vthread"))
+    s[output].bind(vy, te.thread_axis("vthread"))
+    s[output].bind(vx, te.thread_axis("vthread"))
+    s[output].bind(tf, te.thread_axis("threadIdx.z"))
+    s[output].bind(ty, te.thread_axis("threadIdx.y"))
+    s[output].bind(tx, te.thread_axis("threadIdx.x"))
+    s[output].reorder(n, bf, by, bx, vf, vy, vx, tf, ty, tx, fi, yi, xi)
+    s[OL].compute_at(s[output], tx)
+
+    # tile reduction axes
+    n, f, y, x = s[OL].op.axis
+    rc, ry, rx = s[OL].op.reduce_axis
+    rco, rcm, rci = cfg["tile_rc"].apply(s, OL, rc)
+    ryo, rym, ryi = cfg["tile_ry"].apply(s, OL, ry)
+    rxo, rxm, rxi = cfg["tile_rx"].apply(s, OL, rx)
+    s[OL].reorder(rco, ryo, rxo, rcm, rym, rxm, rci, ryi, rxi, n, f, y, x)
+    s[AA].compute_at(s[OL], rxo)
+    s[WW].compute_at(s[OL], rxo)
+    s[AL].compute_at(s[OL], rxm)
+    s[WL].compute_at(s[OL], rxm)
+
+    # cooperative fetching
+    for load in [AA, WW]:
+        n, f, y, x = s[load].op.axis
+        fused = s[load].fuse(n, f, y, x)
+        tz, fused = s[load].split(fused, nparts=cfg["tile_f"].size[2])  # tf
+        ty, fused = s[load].split(fused, nparts=cfg["tile_y"].size[2])  # ty
+        tx, fused = s[load].split(fused, nparts=cfg["tile_x"].size[2])  # tx
+        s[load].bind(tz, te.thread_axis("threadIdx.z"))
+        s[load].bind(ty, te.thread_axis("threadIdx.y"))
+        s[load].bind(tx, te.thread_axis("threadIdx.x"))
+
+    return s, [input, weight, output]
+
+class GroupConvDenseScheduler(ConvDenseScheduler):
+    def __init__(self, channel_out, channel_in, width, kernel, group, save_path):
+        # group = min(group, channel_in)
+        # group = min(group, channel_out)
+        # in case group is not expected
+        assert channel_in % group == 0
+        assert channel_out % group == 0
+        super().__init__(channel_out, channel_in, width, kernel, save_path)
+        self.group = group
+
+    def __repr__(self) -> str:
+        return "GroupConvDenseScheduler"
+
+    @property
+    def _task_name(self):
+        return "dynconv/group_conv_dense"
+
+    @property
+    def _task_args(self):
+        return [self.channel_out, self.channel_in, self.width, self.width, self.kernel, self.kernel, self.group]
+
+    @property
+    def _schedule(self):
+        return schedule_group_conv_dense(*self._task_args)
+
+    def _generate_sample(self):
+        H, W = self.width, self.width
+        KH, KW = self.kernel, self.kernel
+        Cout, Cin = self.channel_out, self.channel_in
+        group = self.group
+        input = np.random.randn(1, Cin, H, W).astype("float32")
+        weight = np.random.randn(Cout, Cin//group, KH, KW).astype("float32")
+        return [input, weight]
+
+    def _convert_sample(self, sample):
+        return super()._convert_sample(sample)
+
+    def _run_tvm(self, inputs):
+        return super()._run_tvm(inputs)
+
+    def _run_numpy(self, inputs):
+        raise NotImplementedError
+
+    def _run_pytorch(self, inputs):
+        input, weight = inputs
+        input = torch.tensor(input, dtype=torch.float32, device="cuda")
+        weight = torch.tensor(weight, dtype=torch.float32, device="cuda")
+        output = F.conv2d(input, weight, groups=self.group)
         output = output.cpu().numpy()
         return output
